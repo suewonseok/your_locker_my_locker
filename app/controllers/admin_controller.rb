880001fbac604396bd4454f41995f26f430af91a
suewonseok <dydrlaks@likelion.org>
2017-07-25 21:31:10 +0900

    관리자 권한 삭제 기능 추가_메시지 추가

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
     @user_admin = User.all.order('stuN ASC')
   end
 
-  def destroy
+  def destroy ##특정 1명의 개인의 계정을 삭제하는 액션
     user = User.find(params[:id])
     if user.admin != true
       user.destroy
@@ -13,7 +13,7 @@ class AdminController < ApplicationController
     redirect_to admin_index_path
   end
 
-  def destroy_all
+  def destroy_all ##관리자 계정을 제외한 모든 계정 초기화 액션
     User.where(admin: nil).destroy_all
     flash[:caution] = "전체 계정 정보가 초기화 되었습니다."
     redirect_to admin_index_path
